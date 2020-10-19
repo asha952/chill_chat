fade3959e0147cfc8f69a1badc28b969c2884a3d
Ashab_Siddiqui <ashabsiddiqui952@gmail.com>
2020-10-18 21:15:44 -0700

    added websocket

---
+++ public/index.js
@@ -0,0 +1,15 @@
+// index.js
+$(document).ready(()=>{
+  const socket = io.connect();
+
+  $('#create-user-btn').click((e)=>{
+    e.preventDefault();
+    let username = $('#username-input').val();
+    if(username.length > 0){
+      //Emit to the server the new user
+      socket.emit('new user', username);
+      $('.username-form').remove();
+    }
+  });
+
+})
